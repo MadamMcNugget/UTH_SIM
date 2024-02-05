c0f1007462f90ebc84540b8d0a83999973a200fb
daza <zephronica@gmail.com>
2024-02-04 17:55:00 -0800

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { TableComponent } from './table/table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', component: TableComponent },
+];
+
+/* for reference only
+
+https://angular.io/guide/router
+
+const routes: Routes = [
+  { path: 'first-component', component: FirstComponent },
+  { path: 'second-component', component: SecondComponent },
+];
+
+html
+<nav>
+  <ul>
+    <li><a routerLink="/first-component" routerLinkActive="active" ariaCurrentWhenActive="page">First Component</a></li>
+    <li><a routerLink="/second-component" routerLinkActive="active" ariaCurrentWhenActive="page">Second Component</a></li>
+  </ul>
+</nav>
+
+*/
